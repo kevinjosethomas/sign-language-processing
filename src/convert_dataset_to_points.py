84950b25afd3e8ff5c4d1ae73ac498b374ec4f60
Kevin Thomas <kevin.jt2007@gmail.com>
2024-04-22 16:22:29 -0700

    feat: data augmentation functions

--- src/convert_dataset_to_points.py
+++ src/convert_dataset_to_points.py
@@ -8,7 +8,6 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 
-# Initialize Model
 base_options = python.BaseOptions(model_asset_path="../mediapipe.task")
 options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=1)
 detector = vision.HandLandmarker.create_from_options(options)
@@ -33,11 +32,10 @@ def vectorize_image(image_path: str, index: int):
 
     points = np.array(points)
 
-    # Normaliza data into a standard range
-    min_x = np.min(points[:, 0])  # Find lowest x-value
-    max_x = np.max(points[:, 0])  # Find highest x-value
-    min_y = np.min(points[:, 1])  # Find lowest y-value
-    max_y = np.max(points[:, 1])  # Find highest y-value
+    min_x = np.min(points[:, 0])
+    max_x = np.max(points[:, 0])
+    min_y = np.min(points[:, 1])
+    max_y = np.max(points[:, 1])
     for i in range(len(points)):
         points[i][0] = (points[i][0] - min_x) / (max_x - min_x)
         points[i][1] = (points[i][1] - min_y) / (max_y - min_y)
