ea4a8413d570d0c770a0ea5f270ce76008f49bc7
Kevin Thomas <kevin.jt2007@gmail.com>
2024-04-23 10:10:21 -0700

    feat: modified to merge hands

--- scripts/create_dataset_directories.py
+++ scripts/create_dataset_directories.py
@@ -3,7 +3,4 @@ import os
 ALPHABET = "ABCDEFGHIKLMNOPQRSTUVWXY"
 
 for letter in ALPHABET:
-    os.makedirs(f"../data/dataset/left/test/{letter}", exist_ok=True)
-    os.makedirs(f"../data/dataset/left/train/{letter}", exist_ok=True)
-    os.makedirs(f"../data/dataset/right/test/{letter}", exist_ok=True)
-    os.makedirs(f"../data/dataset/right/train/{letter}", exist_ok=True)
+    os.makedirs(f"../data/merged/{letter}", exist_ok=True)
