8b9e70d292ace3bd2ac790b8a0143596a36ed20e
Kevin Thomas <kevin.jt2007@gmail.com>
2024-04-29 00:11:33 -0700

    feat: added iframe functionality

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -1,77 +1,14 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import io from "socket.io-client";
-import Peer from "simple-peer";
 
 const socket = io("http://localhost:1234");
 
-const VideoComponent = () => {
-  const localRef = useRef();
-  const remoteRef = useRef();
-  const peerRef: any = useRef(null);
-
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          width: { ideal: 4096 },
-        },
-      })
-      .then((stream) => {
-        if (localRef.current) {
-          localRef.current.srcObject = stream;
-        }
-
-        const peer = new Peer({
-          initiator: true,
-          trickle: false,
-          stream: stream,
-          config: {
-            iceServers: [],
-          },
-        });
-
-        peerRef.current = peer;
-
-        peer.on("signal", (data) => {
-          socket.emit("offer", data);
-        });
-
-        socket.on("answer", (data) => {
-          peer.signal(data);
-        });
-
-        peer.on("stream", (s) => {
-          if (remoteRef.current) {
-            remoteRef.current.srcObject = s;
-          }
-        });
-      });
-  }, []);
-
+export default function Home() {
   return (
     <div>
-      <div className="w-screen">
-        <video
-          ref={localRef}
-          className="z-10 w-full hidden"
-          autoPlay
-          muted
-        ></video>
-        <video
-          ref={remoteRef}
-          className="z-20 w-full"
-          autoPlay
-          playsInline
-        ></video>
-        <iframe
-          src="http://localhost:5001/video"
-          className="w-screen h-screen"
-        />
-      </div>
+      <iframe src="http://localhost:1234" className="w-screen h-screen" />
     </div>
   );
-};
-
-export default VideoComponent;
+}
--- src/server/server.py
+++ src/server/server.py
@@ -1,13 +1,10 @@
-import asyncio
-import socketio
-from aiohttp import web
+from flask import Flask, render_template, Response
 import os
 import cv2
 import threading
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from av import VideoFrame
 
 from landmarker import Landmarker
 from classifier import Classifier
@@ -20,98 +17,144 @@ llm = ChatOpenAI(
     model="gpt-3.5-turbo-0125",
     openai_api_key=os.getenv("OPENAI_API_KEY"),
 )
-from aiortc import (
-    MediaStreamTrack,
-    RTCPeerConnection,
-    RTCSessionDescription,
-    RTCConfiguration,
-    RTCIceServer,
+
+PROMPT = ChatPromptTemplate.from_messages(  # The alphabets that the software often gets confused between are: A, S, T, N and M; G and H; D and I; F and W; P and Q; R and U.
+    [
+        (
+            "system",
+            "You are an LLM meant to fix typos in given phrases. I will send you a "
+            "phrase, please correct it if there are any typos. These are the alphabets that are mistaken for each other the most often: A and S and T and N and M. G and H. D and I. F and W. P and Q. R and U. "
+            "Please output nothing but the corrected phrase. No punctuation. Make sure everything is an English word"
+            "or a proper noun. Make sure the phrase makes sense.",
+        ),
+        ("human", "{transcription}"),
+    ]
 )
-from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
+chain = PROMPT | llm
 
-sio = socketio.AsyncServer(cors_allowed_origins="*")
-app = web.Application()
-sio.attach(app)
-relay = MediaRelay()
-peer_connections = {}
+transcription_current_word = ""
+ai_transcription_words = []
+transcription_words = []
+transcription_alphabet_log = []
 
-
-class VideoTransformTrack(MediaStreamTrack):
-    """
-    A video stream track that transforms frames from an another track.
-    """
-
-    kind = "video"
-
-    def __init__(self, track):
-        super().__init__()  # don't forget this!
-        self.track = track
-
-    async def recv(self):
-        frame = await self.track.recv()
-        img = frame.to_ndarray(format="bgr24")
-        success, image, points, first_landmark = landmarker.process(img)
-        if success:
-            new_frame = VideoFrame.from_ndarray(image, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        else:
-            return frame
+app = Flask(__name__)
+camera = cv2.VideoCapture(2)
 
 
-@sio.event
-async def offer(sid, data):
-    offer = RTCSessionDescription(sdp=data["sdp"], type=data["type"])
-    peer = RTCPeerConnection(
-        configuration=RTCConfiguration(
-            iceServers=[RTCIceServer(urls="stun:localhost:19302")]
-        )
-    )
-    peer_connections[sid] = peer
+def fix_transcription():
+    global transcription_words, transcription_current_word, transcription_alphabet_log, ai_transcription_words
 
-    print("Created Peer Connection")
-
-    @peer.on("connectionstatechange")
-    async def on_connectionstatechange():
-        print("Connection state is ", peer.connectionState)
-        if peer.connectionState == "failed":
-            await peer.close()
-            del peer_connections[sid]
-
-    @peer.on("signal")
-    async def on_signal(signal):
-        print("\nReceived signal\n")
-        await sio.emit("signal", signal, to=sid)
-
-    @peer.on("track")
-    def on_track(track):
-
-        if track.kind == "video":
-            peer.addTrack(VideoTransformTrack(relay.subscribe(track)))
-
-        @track.on("ended")
-        async def on_ended():
-            print("Track %s ended", track.kind)
-
-    # @peer.on("candidate")
-    # async def on_candidate(candidate):
-    #     print("\nReceived candidate\n")
-    #     await sio.emit("candidate", candidate, to=sid)
-
-    await peer.setRemoteDescription(offer)
-    answer = await peer.createAnswer()
-    await peer.setLocalDescription(answer)
-
-    await sio.emit(
-        "answer",
+    ai_transcription_words.append(transcription_current_word)
+    transcription_current_word = ""
+    transcription = " ".join(transcription_words)
+    response = chain.invoke(
         {
-            "sdp": peer.localDescription.sdp,
-            "type": peer.localDescription.type,
-        },
-        to=sid,
+            "transcription": transcription,
+        }
+    )
+
+    print(" ".join(transcription_words))
+    ai_transcription_words = response.content.strip().upper().split()
+
+
+def generate_frames():
+    global transcription_words, transcription_current_word, transcription_alphabet_log, ai_transcription_words
+    while True:
+
+        while camera.isOpened():
+
+            success, img = camera.read()
+            if not success:
+                continue
+
+            success, image, points, first_landmark = landmarker.process(img)
+
+            if success:
+                letter, probability = classifier.classify(points)
+                transcription_alphabet_log.append(letter)
+
+                if probability > 0.80:
+                    transcribed = False
+
+                    # 1. Ensure that the last 20 letters are the same
+                    # 2. Ensure that it does not repeat the same character more than twice
+                    longer_recent = set(transcription_alphabet_log[-20:])
+                    if len(longer_recent) == 1 and (
+                        len(transcription_current_word) < 2
+                        or transcription_current_word[-2:] != letter * 2
+                    ):
+                        transcription_current_word += letter
+                        transcribed = True
+                    else:
+                        # Ensure that the last 4 letters are the same
+                        # Ensure that it does not repeat the same character more than once
+                        recent = set(transcription_alphabet_log[-4:])
+                        if len(recent) == 1 and (
+                            not transcription_current_word
+                            or transcription_current_word[-1] != letter
+                        ):
+                            transcription_current_word += letter
+                            transcribed = True
+
+                    height, width, _ = image.shape
+                    text_x = int(first_landmark[0] * width) - 100
+                    text_y = int(first_landmark[1] * height) + 50
+                    cv2.putText(
+                        img=image,
+                        text=f"{letter} {round(probability * 100 * 100) / 100}%",
+                        org=(text_x, text_y),
+                        fontFace=cv2.FONT_HERSHEY_PLAIN,
+                        fontScale=5,
+                        color=(0, 0, 255) if transcribed else (0, 255, 0),
+                        thickness=4,
+                        lineType=cv2.LINE_4,
+                    )
+            else:
+                if transcription_current_word:
+                    transcription_words.append(transcription_current_word)
+
+                    new_thread = threading.Thread(target=fix_transcription)
+                    new_thread.start()
+
+            output = (
+                " "
+                + (
+                    " ".join(ai_transcription_words) + " " + transcription_current_word
+                ).strip()
+                + " "
+            )
+            text_width = cv2.getTextSize(output, cv2.FONT_HERSHEY_DUPLEX, 2, 5)[0][0]
+            pos = ((image.shape[1] - text_width) // 2, image.shape[0] - 30)
+            cv2.rectangle(
+                image,
+                (0, image.shape[0] - 100),
+                (image.shape[1], image.shape[0]),
+                (255, 255, 255),
+                -1,
+            )
+            cv2.putText(
+                img=image,
+                text=output,
+                org=pos,
+                fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                fontScale=2,
+                color=(0, 0, 0),
+                thickness=5,
+                lineType=cv2.LINE_4,
+            )
+
+            _, buffer = cv2.imencode(".jpg", image)
+            frame = buffer.tobytes()
+
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/")
+def video():
+    return Response(
+        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
     )
 
 
 if __name__ == "__main__":
-    web.run_app(app, port=1234)
+    app.run(debug=True, port=1234)
