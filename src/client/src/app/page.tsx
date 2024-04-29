6e0032c79ca95b4978d2ae243e7f04ee2b4e3af6
Kevin Thomas <kevin.jt2007@gmail.com>
2024-04-29 11:48:01 -0700

    minor: added state storage for transcription

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
 import React from "react";
-import { useEffect } from "react";
 import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 const socket = io("http://localhost:1234");
 
 export default function Home() {
+  const [transcription, setTranscription] = useState("");
+
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Connected to server");
     });
 
     socket.on("transcription", (data) => {
-      console.log(data);
+      setTranscription(data);
     });
   });
   return (
-    <div>
-      <iframe src="http://localhost:1234" className="w-screen h-screen" />
+    <div className="w-screen h-screen">
+      <iframe src="http://localhost:1234" className="w-full h-screen" />
     </div>
   );
 }
