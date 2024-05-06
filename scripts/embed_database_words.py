6b4fa79ef4889b5071b4f010183609117732e267
Kevin Thomas <kevin.jt2007@gmail.com>
2024-05-06 16:01:31 -0700

    feat: added script to generate embeddings of every word

---
+++ scripts/embed_database_words.py
@@ -0,0 +1,26 @@
+import tqdm
+import psycopg2
+import numpy as np
+from pgvector.psycopg2 import register_vector
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+conn = psycopg2.connect(
+    database="signs",
+    host="localhost",
+    user="postgres",
+    port=5432,
+)
+register_vector(conn)
+cur = conn.cursor()
+
+cur.execute("SELECT word FROM signs")
+words = cur.fetchall()
+words = [word[0] for word in words]
+bar = tqdm.tqdm(total=len(words))
+for word in words:
+    embedding = model.encode(word)
+    cur.execute("UPDATE signs SET embedding = %s WHERE word = %s", (embedding, word))
+    conn.commit()
+
+    bar.update(1)
