d1b04236aa0886b0e666990553fd06b600004442
Kevin Thomas <kevin.jt2007@gmail.com>
2024-07-10 10:49:16 -0700

    feqt: fixed pgvector errors & added new models

--- src/server/utils/classifier.py
+++ src/server/utils/classifier.py
@@ -6,10 +6,10 @@ LETTERS = "ABCDEFGHIKLMNOPQRSTUVWXY"
 
 class Classifier:
     def __init__(self):
-        self.model = keras.models.load_model("model.keras")
+        self.model = keras.models.load_model("model4.keras")
 
     def classify(self, points):
-        predictions = self.model.predict(points, verbose=0)
+        predictions = self.model.predict(points[:, :, :2], verbose=0)
         prediction = ops.argmax(predictions, -1)
         probability = predictions[0][prediction[0]]
         letter = LETTERS[prediction[0]]
